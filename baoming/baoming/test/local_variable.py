4149a085b7196f451673a09e10bde2e9e7e4ab66
hanxiaoshun <18301513217@sina.cn>
2020-05-25 08:48:27 +0800

    变量学习测试

---
+++ baoming/baoming/test/local_variable.py
@@ -0,0 +1,21 @@
+
+# if 1 == 1:
+#     a = 'a'
+    
+# print(a)
+# if 2 == 2:
+#     a = 'b'
+#     print(a)
+    
+# print(a)
+
+def love():
+    if 1 == 1:
+        a = 'a'
+    girl = {'a':a}
+    return girl
+
+def you():
+    print(love())
+
+you()
